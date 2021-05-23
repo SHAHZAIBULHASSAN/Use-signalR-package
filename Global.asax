ceb32b4d1667352aa18e92df49ae2cec5c55b945
SHAHZAIB <ranashahzaibulhassan@gmail.com>
2021-05-23 16:48:47 +0500

    Add files via upload

---
+++ Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="signal_hour.MvcApplication" Language="C#" %>
---
+++ Global.asax.cs
@@ -0,0 +1,22 @@
+﻿using System.Web.Mvc;
+using System.Web.Optimization;
+using System.Web.Routing;
+
+namespace signal_hour
+{
+    public class MvcApplication : System.Web.HttpApplication
+    {
+        protected  void Application_Start()
+        {
+            AreaRegistration.RegisterAllAreas();
+            RouteTable.Routes.Clear();
+         
+            FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
+            RouteConfig.RegisterRoutes(RouteTable.Routes);
+            BundleConfig.RegisterBundles(BundleTable.Bundles);
+             
+        
+        
+        }
+    }
+}
